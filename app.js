fd84b8ce1ebb0b0bebaa3e0cfbeb8f43da15d5a5
Salman Khan <skhan231714@gmail.com>
2023-03-26 23:53:57 +0500

    Second Session

--- app.js
+++ app.js
@@ -1,20 +1,44 @@
 var input1 = document.getElementById('num1');
 var input2 = document.getElementById('num2');
 var btn = document.querySelector('#btn');
-var add = function (val1, val2) {
-    return val1 + val2;
-};
-btn.addEventListener('click', function () {
-    var firstVal = input1.value;
-    var secondVal = input2.value;
-    console.log(add(+firstVal, +secondVal));
-});
+// const add=function(val1:number,val2:number):number{
+//     return val1+val2;
+// }
+// btn.addEventListener('click',function(){
+//     const firstVal=input1.value;
+//     const secondVal=input2.value;
+//     console.log(add(+firstVal,+secondVal));
+// })
 //Basic Types
 // 1.String:string=>"Apple","Banana","Orange"
 // 2.Number:number=>"1","2","3"
 // 3.Boolean:boolean=>true,false
-var person = "salman";
-// const arr=["Apple","orange"];
-var arr = ["Apple", "orange"];
-arr.push('Banana');
+// let person="salman"
+// const arr:string[]=["Apple","orange"];
+// arr.push('Banana');
 var arr2 = [1, 2, 3];
+var person = {
+    name: "salman",
+    age: 20,
+    hobbies: ["Sports", "Study"]
+};
+var persons = [];
+persons.push({
+    name: "Hussain",
+    age: 25,
+    hobbies: ["Sports", "Gaming"]
+});
+// const add=function(val1:number,val2:number, cb:(x:string)=>void):number{
+//    cb("hello");
+//     return val1+val2;
+// let func:(x:number,y:number,z:(x:string)=>void)=>number;
+var func; // func=add;
+var add = function (val1, val2, calc) {
+    if (calc === "add" || calc === "+") {
+        return val1 + val2;
+    }
+    else if (calc === "subtract" || calc === "-") {
+        return val1 - val2;
+    }
+};
+console.log(add(1, 3, "-"));
--- app.ts
+++ app.ts
@@ -2,24 +2,57 @@ const input1=document.getElementById('num1')! as HTMLInputElement;
 const input2=document.getElementById('num2')! as HTMLInputElement;
 const btn=document.querySelector('#btn')!;
 
-const add=function(val1:number,val2:number){
-    return val1+val2;
-}
-btn.addEventListener('click',function(){
-    const firstVal=input1.value;
-    const secondVal=input2.value;
-    console.log(add(+firstVal,+secondVal));
-})
+// const add=function(val1:number,val2:number):number{
+//     return val1+val2;
+// }
+// btn.addEventListener('click',function(){
+//     const firstVal=input1.value;
+//     const secondVal=input2.value;
+//     console.log(add(+firstVal,+secondVal));
+// })
 
 //Basic Types
 // 1.String:string=>"Apple","Banana","Orange"
 // 2.Number:number=>"1","2","3"
 // 3.Boolean:boolean=>true,false
-let person="salman"
-const arr:string[]=["Apple","orange"];
-arr.push('Banana');
+// let person="salman"
+// const arr:string[]=["Apple","orange"];
+// arr.push('Banana');
 
 const arr2:number[]=[1,2,3];
 
+// Objects 
 
+type Person={
+    name:String;
+    age:Number;
+    hobbies:String[];
+    email?:string;// optional field ky liey ? lagaty hy
+};
 
+let person:Person={
+    name:"salman", //key:value
+    age:20,
+    hobbies:["Sports","Study"]
+};
+
+const persons:Person[]=[];
+persons.push({
+    name:"Hussain", //key:value
+    age:25,
+    hobbies:["Sports","Gaming"]
+});
+// const add=function(val1:number,val2:number, cb:(x:string)=>void):number{
+//    cb("hello");
+//     return val1+val2;
+// let func:(x:number,y:number,z:(x:string)=>void)=>number;
+let func:(x:number,y:number,calc:number)=>number;// func=add;
+type Cals="add"|"subtract"|"+"|"-"
+const add=function(val1:number,val2:number,calc:Cals){
+   if(calc==="add"||calc==="+"){
+       return val1+val2
+   }else if(calc==="subtract"||calc==="-"){
+    return val1-val2;
+   }
+}
+console.log(add(1,3,"-"))
